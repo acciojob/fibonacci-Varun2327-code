a43a9155ebd3ce3784eca146c08ad025d6f56e71
Varun shrimal <varunshrimal07@gmail.com>
2025-05-12 10:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function fibonacci(num) {
-// your code here
+function mincost(arr) {
+    // Min heap using simple array with sorting
+    let heap = [...arr].sort((a, b) => a - b); // initialize min-heap
+
+    let totalCost = 0;
+
+    while (heap.length > 1) {
+        // Always take two smallest ropes
+        let first = heap.shift();
+        let second = heap.shift();
+
+        let cost = first + second;
+        totalCost += cost;
+
+        // Insert the new rope back into the heap
+        heap.push(cost);
+        heap.sort((a, b) => a - b); // Re-sort to maintain min-heap behavior
+    }
+
+    return totalCost;
 }
 
-module.exports = fibonacci;
+module.exports = mincost;
