f000a0d105f77005b28cc9b0be0875ecdc61805e
Varun shrimal <varunshrimal07@gmail.com>
2025-05-12 09:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 function fibonacci(num) {
-  if (num === 1) return 0;
-  if (num === 2) return 1;
-
-  let a = 0, b = 1;
-  for (let i = 3; i <= num; i++) {
-    let temp = a + b;
-    a = b;
-    b = temp;
-  }
-  return b;
+// your code here
 }
+
+module.exports = fibonacci;
